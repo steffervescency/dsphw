f4424f9f15ff24828c5e56b3ea8b874f1b3fcbea
Aljoscha Dietrich <aljoscha.dietrich@gmail.com>
2015-06-17 01:23:03 +0200

    Task 1.2

---
+++ 5/MSE.m
@@ -0,0 +1,22 @@
+function [ e_l ] = MSE( l )
+data = dlmread('alo.txt', ',', 0, 1);
+[P,V] = PCA(data);
+
+D_e = mean(data);
+
+P_l = P(:, 1:(size(P,2)-l));
+P_l_trans = P_l';
+
+i = 1;
+while i <= 16
+    x = data(i,:)';
+    y = P_l_trans * x;  
+    sum = abs(x-(P_l*y+D_e')).^2;
+    i = i+1;
+end
+e_l = sum/16;
+plot(e_l);
+
+
+end
+
