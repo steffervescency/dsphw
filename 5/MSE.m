e5b68552763eeee5b6ed6f93f4e7c6413eb1999a
Aljoscha Dietrich <aljoscha.dietrich@gmail.com>
2015-06-17 07:58:02 +0200

    Correcting the sum function; making it more general

--- 5/MSE.m
+++ 5/MSE.m
@@ -6,12 +6,14 @@ D_e = mean(data);
 
 P_l = P(:, 1:(size(P,2)-l));
 P_l_trans = P_l';
+sum = 0;
+N = size(data,2);
 
 i = 1;
-while i <= 16
+while i <= N
     x = data(i,:)';
     y = P_l_trans * x;  
-    sum = abs(x-(P_l*y+D_e')).^2;
+    sum = sum + abs(x-(P_l*y+D_e')).^2;
     i = i+1;
 end
 e_l = sum/16;
