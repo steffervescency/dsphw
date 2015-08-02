c40e5395bc822e23be4e335ef2d3515cda7b011b
Neo <varanasi.stalin@gmail.com>
2015-08-02 08:29:41 +0200

    Edited hw9.m and adding hw9.pdf

--- 9/hw9.m
+++ 9/hw9.m
@@ -1,7 +1,7 @@
 % 1.1 Read and play back file
 [sound1, fs] = audioread('sound1.wav');
-% sound(sound1, fs);
 
+% sound(sound1, fs);
 % 1.2 Pre-emphasis filter
 y = pre(sound1(:, 1));
 
@@ -10,20 +10,27 @@ M = windowing(y, ceil(fs*10/1000), ceil(fs*25/1000));
 
 % 1.4 Mel filterbank values
 mels = mel(133.33334, 6855.4976, 1024, 16000, 24, 1125);
-%plot([1:100], mels(:, 1:100))
+
+plot([1:100], mels(:, 1:100))
+figure;
+
 
 % 1.5 Compute MFCC
 % Apply FFT to each window
-
+FFTM= fft(M,1024);        % taking fft_size=1024 for the FFT
+FFTM_trunc=FFTM(1:512,:); % Taking only the first half as the second half is the same
 
 % Apply Mel filterbank 
-% TODO
+melfiltered=mels*abs(FFTM_trunc);
+
 
 % Apply logarithm, DCT to get the MFCC
-% TODO
+MFCC=dct(log10(melfiltered));
 
-% Visualize result
-% TODO
+% Visualize resultffty
+imagesc(MFCC);
+figure;
 
 % 1.6 Plot original signal, spectrogram, and MFCC spectrogram
-% TODO
+spectrogram(sound1(:,1),128,120,128,1e3);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
